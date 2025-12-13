f53fd3f330219ce5f7340fd95e35a6ec2508ce0c
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2025-12-13 18:37:24 +0900

    update(version): Get version from build info
    - Read application version from debug.ReadBuildInfo()

--- cmd/version.go
+++ cmd/version.go
@@ -7,21 +7,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	version string
-)
+var version = "devel"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show the version of leetcode-cli(lcli)",
+	Short: "Show the version of lcli(leetcode-cli)",
 	RunE: func(_ *cobra.Command, _ []string) error {
-		fmt.Printf("leetcode-cli(lcli) version %s\n", version)
+		fmt.Printf("lcli version %s\n", version)
 		return nil
 	},
 }
 
 func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	mainVersion := info.Main.Version
+	if mainVersion != "" && mainVersion != "(devel)" {
+		version = mainVersion
+	}
 	if info, ok := debug.ReadBuildInfo(); ok {
 		version = info.Main.Version
 	}
